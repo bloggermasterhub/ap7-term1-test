67e2aeeb58108aab68ee5d88546be5f68c17fdd2
bloggermasterhub <79012517+bloggermasterhub@users.noreply.github.com>
2026-07-19 21:00:50 +0800

    Update ap7-term1-st1.js

--- tests/ap7-term1-st1.js
+++ tests/ap7-term1-st1.js
@@ -51,10 +51,10 @@ window.summativeTest1Questions = [
     id: 9,
     question: "Bakit tinatawag na bansang insular ang Pilipinas?",
     choices: [
-        "Binubuo ito ng magkakadikit na kalupaan",
+        "Binubuo ito ng magkakadugtong na kalupaan sa iisang rehiyon",
         "Binubuo ito ng maraming pulong napapalibutan ng tubig",
-        "Binubuo ito ng malalawak na lambak",
-        "Binubuo ito ng magkakaugnay na tangway"
+        "Binubuo ito ng malalawak na lambak at kabundukan",
+        "Binubuo ito ng magkakaugnay na tangway at kapatagan"
     ],
     correctAnswer: "B"
 },
@@ -62,10 +62,10 @@ window.summativeTest1Questions = [
     id: 10,
     question: "Bakit madalas makaranas ng lindol ang Pilipinas?",
     choices: [
-        "Dahil sa monsoon winds",
+        "Dahil sa paggalaw ng hanging habagat at amihan sa rehiyon",
         "Dahil sa aktibong tectonic activity",
-        "Dahil sa malaking populasyon",
-        "Dahil sa malawak na kagubatan"
+        "Dahil sa mabilis na paglaki ng populasyon sa bansa",
+        "Dahil sa lawak ng kagubatan at yamang likas nito"
     ],
     correctAnswer: "B"
 },
@@ -73,10 +73,10 @@ window.summativeTest1Questions = [
     id: 11,
     question: "Bakit sagana ang rainforest sa Timog-Silangang Asya?",
     choices: [
-        "Dahil sa makapal na niyebe",
+        "Dahil sa pag-ulan ng niyebe sa maraming bahagi ng taon",
         "Dahil sa sapat na init at pag-ulan taon-taon",
-        "Dahil sa mahabang taglamig",
-        "Dahil sa tuyong klima"
+        "Dahil sa mahabang panahon ng malamig na taglamig",
+        "Dahil sa kakulangan ng ulan sa karamihan ng lugar"
     ],
     correctAnswer: "B"
 },
@@ -85,9 +85,9 @@ window.summativeTest1Questions = [
     question: "Bakit mahalaga ang yamang gubat sa rehiyon?",
     choices: [
         "Tirahan ito ng mga hayop at pinagkukunan ng kahoy",
-        "Sentro ito ng industriya",
-        "Nagpaparami ito ng komersyal na establisimyento",
-        "Nagpapalawak ito ng urbanisasyon"
+        "Sentro ito ng mga pabrika at gawaing pang-industriya",
+        "Nagbibigay ito ng lugar para sa mas maraming establisimyento",
+        "Nagpapabilis ito ng paglawak ng mga lungsod at bayan"
     ],
     correctAnswer: "A"
 },
@@ -96,9 +96,9 @@ window.summativeTest1Questions = [
     question: "Bakit sagana sa likas na yaman ang Timog-Silangang Asya?",
     choices: [
         "Dahil sa tropikal na klima at lokasyon nito",
-        "Dahil sa malawak na disyerto",
-        "Dahil sa mataas na industriyalisasyon",
-        "Dahil sa kaunting populasyon"
+        "Dahil sa malawak na disyerto at tuyong kapaligiran",
+        "Dahil sa mataas na antas ng industriyalisasyon nito",
+        "Dahil sa mababang bilang ng mamamayan sa rehiyon"
     ],
     correctAnswer: "A"
 },
@@ -107,9 +107,9 @@ window.summativeTest1Questions = [
     question: "Ano ang pangunahing pagkakaiba ng biotic at abiotic resources?",
     choices: [
         "Ang biotic ay buhay at ang abiotic ay di-buhay",
-        "Ang biotic ay mineral at ang abiotic ay halaman",
-        "Ang biotic ay gawa ng tao",
-        "Pareho silang di-buhay"
+        "Ang biotic ay yamang mineral at ang abiotic ay halaman",
+        "Ang biotic ay karaniwang gawa ng tao para sa paggamit",
+        "Parehong kabilang sa mga bagay na walang buhay lamang"
     ],
     correctAnswer: "A"
 },
@@ -117,10 +117,10 @@ window.summativeTest1Questions = [
     id: 15,
     question: "Ano ang pangunahing layunin ng likas-kayang pag-unlad?",
     choices: [
-        "Paramihin ang industriya",
-        "Palawakin ang pagmimina",
+        "Paramihin ang mga industriya sa lahat ng sektor ng ekonomiya",
+        "Palawakin ang gawaing pagmimina sa iba't ibang rehiyon",
         "Pangalagaan ang kalikasan habang natutugunan ang pangangailangan ng tao",
-        "Pataasin ang kita ng mga negosyo"
+        "Pataasin ang kita ng mga negosyo at malalaking korporasyon"
     ],
     correctAnswer: "C"
 },
@@ -128,10 +128,10 @@ window.summativeTest1Questions = [
     id: 16,
     question: "Naligaw ka sa isang lugar at may dala kang mapa. Ano ang pinakamahalagang kaalaman na dapat gamitin?",
     choices: [
-        "Pagkilala sa populasyon",
-        "Pag-aaral ng kasaysayan",
+        "Pagkilala sa dami ng tao at kabahayan sa lugar",
+        "Pag-aaral ng kasaysayan at kultura ng pamayanan",
         "Pagtukoy sa direksyon at lokasyon",
-        "Pagkilala sa kultura"
+        "Pagkilala sa tradisyon at pamumuhay ng mga tao"
     ],
     correctAnswer: "C"
 },
@@ -139,10 +139,10 @@ window.summativeTest1Questions = [
     id: 17,
     question: "Paano makatutulong ang kaalaman sa klima sa mga magsasaka?",
     choices: [
-        "Matutukoy ang presyo ng produkto",
+        "Matutukoy ang kasalukuyang presyo ng mga produkto sa pamilihan",
         "Matutukoy ang wastong panahon ng pagtatanim",
-        "Matutukoy ang populasyon",
-        "Matutukoy ang lawak ng lupa"
+        "Matutukoy ang bilang ng mamamayan sa kanilang komunidad",
+        "Matutukoy ang kabuuang lawak ng mga lupang sakahan"
     ],
     correctAnswer: "B"
 },
@@ -150,10 +150,10 @@ window.summativeTest1Questions = [
     id: 18,
     question: "Ano ang dapat gawin upang mapangalagaan ang likas na yaman?",
     choices: [
-        "Magmina sa protektadong lugar",
-        "Palawakin ang pagtotroso",
+        "Magmina nang mas marami sa mga protektadong kagubatan",
+        "Palawakin ang pagtotroso sa iba't ibang bulubundukin",
         "Magsagawa ng pagtatanim at wastong paggamit ng yaman",
-        "Magsunog ng basura"
+        "Magsunog ng mga basurang nakakalat sa kapaligiran"
     ],
     correctAnswer: "C"
 },
@@ -161,10 +161,10 @@ window.summativeTest1Questions = [
     id: 19,
     question: "Maraming sakahan sa inyong bayan ang ginawang subdivision. Ano ang posibleng epekto nito?",
     choices: [
-        "Madadagdagan ang lupang sakahan",
-        "Lalawak ang agrikultura",
+        "Madadagdagan ang dami ng lupang maaaring sakahin",
+        "Lalawak ang sektor ng agrikultura at produksyon ng ani",
         "Mababawasan ang lupang maaaring taniman",
-        "Tataas ang produksyon ng pananim"
+        "Tataas ang kabuuang produksyon ng mga pangunahing pananim"
     ],
     correctAnswer: "C"
 },
@@ -172,10 +172,10 @@ window.summativeTest1Questions = [
     id: 20,
     question: "Bakit mas mahalaga ang transportasyong pandagat sa mga bansang insular kaysa mainland?",
     choices: [
-        "Maraming industriya rito",
-        "Mababang populasyon nito",
+        "Maraming sentrong pang-industriya at pangkalakalan dito",
+        "Mas mababa ang bilang ng populasyon sa mga pulo",
         "Binubuo ito ng mga pulong pinagdurugtong ng dagat",
-        "Maraming lambak at kapatagan"
+        "Mas marami itong lambak, kapatagan, at kabundukan"
     ],
     correctAnswer: "C"
 },
@@ -184,9 +184,9 @@ window.summativeTest1Questions = [
     question: "Ano ang kaugnayan ng lokasyon at klima?",
     choices: [
         "Nakaaapekto ang lokasyon sa klima ng isang lugar",
-        "Nakaaapekto ang ekonomiya sa klima",
-        "Nakaaapekto ang populasyon sa klima",
-        "Nakaaapekto ang klima sa lokasyon"
+        "Nakaaapekto ang kalagayang pang-ekonomiya sa klima",
+        "Nakaaapekto ang dami ng tao sa klima ng lugar",
+        "Nakaaapekto ang klima sa kinalalagyang lokasyon"
     ],
     correctAnswer: "A"
 },
@@ -194,10 +194,10 @@ window.summativeTest1Questions = [
     id: 22,
     question: "Patuloy na nababawasan ang kagubatan sa isang rehiyon. Ano ang pinaka-posibleng epekto nito sa biodiversity?",
     choices: [
-        "Dadami ang mga species",
+        "Dadami ang iba't ibang uri ng halaman at hayop sa lugar",
         "Mababawasan ang bilang ng mga halaman at hayop",
-        "Lalawak ang ecosystem",
-        "Magiging mas matatag ang tirahan ng mga hayop"
+        "Lalawak at mas magiging masagana ang ecosystem nito",
+        "Magiging mas matatag ang mga tirahan ng mga hayop"
     ],
     correctAnswer: "B"
 },
@@ -206,9 +206,9 @@ window.summativeTest1Questions = [
     question: "Alin ang pinakamabisang paraan upang mabawasan ang panganib ng lindol at pagputok ng bulkan?",
     choices: [
         "Pagsunod sa hazard map at mga protocol sa kaligtasan",
-        "Pag-asa lamang sa karanasan ng komunidad",
-        "Pananatili sa tahanan kahit may babala",
-        "Pag-iwas sa lahat ng gawaing pangkabuhayan"
+        "Pag-asa lamang sa nakagawiang karanasan ng komunidad",
+        "Pananatili sa loob ng bahay kahit may opisyal na babala",
+        "Pag-iwas sa lahat ng gawaing pangkabuhayan sa lugar"
     ],
     correctAnswer: "A"
 },
@@ -216,10 +216,10 @@ window.summativeTest1Questions = [
     id: 24,
     question: "Alin sa sumusunod ang pinakamahusay na pagpapakita ng pagpapahalaga sa likas na yaman?",
     choices: [
-        "Paggamit ng yaman nang walang limitasyon",
+        "Paggamit ng mga likas na yaman nang walang hangganan",
         "Paggamit ng yaman nang may pananagutan at pag-iingat",
-        "Mabilisang industriyalisasyon",
-        "Pagtuon lamang sa kita"
+        "Mabilisang pagpapatayo ng mga industriya sa rehiyon",
+        "Pagtuon lamang sa pagpapalaki ng kita at produksyon"
     ],
     correctAnswer: "B"
 },
@@ -227,10 +227,10 @@ window.summativeTest1Questions = [
     id: 25,
     question: "Bilang SK Chairperson, alin sa mga sumusunod ang pinakamainam na programang maaari mong buuin upang mapangalagaan ang likas na yaman ng inyong komunidad?",
     choices: [
-        "Programang nakatuon sa pagpapaunlad ng mga gusali",
-        "Programang nakatuon sa pagpapataas ng produksiyong mineral",
+        "Programang nakatuon sa pagpapaunlad ng mga gusali at imprastraktura",
+        "Programang nakatuon sa pagpapataas ng produksiyong mineral sa lugar",
         "Programang nakatuon sa konserbasyon at rehabilitasyon ng kapaligiran",
-        "Programang nakatuon sa pagpapalawak ng paggamit ng lupain"
+        "Programang nakatuon sa pagpapalawak ng paggamit ng lupain sa komunidad"
     ],
     correctAnswer: "C"
 }
